3494bdec9ebf8f7f71069608d24e2af801c76420
it4joy <drkierkegor@gmail.com>
2017-07-05 19:35:59 +0300

    2017-07-05 :: 19:35 // checkin of control number

--- wp-content/plugins/sgforms/sgforms.js
+++ wp-content/plugins/sgforms/sgforms.js
@@ -25,28 +25,33 @@ jQuery(document).ready(function($) {
 	$("#callback-form-ajax").on("submit", function(e) {
 		e.preventDefault();
 		var $form = $(this);
-		$.ajax({
-			method: "POST",
-			url: sg_forms_ajax.ajax_url,
-			data: {
-				action: "sg_ajax",
-				nonce: sg_forms_ajax.nonce,
-				name: $("#name").val(),
-				phone: $("#phone2").val(),
-				checking: $("#checking").val(),
-				agreement: $("#agreement").val(),
-				form_type: $form.find(".form-type").val()
-			},
-			success: function() {
-				$form.trigger("reset");
-				$("#callback-form").modal("hide");
-				$("#success-modal").modal("show");
-			},
-			error: function() {
-				$form.trigger("reset");
-				$("#callback-form").modal("hide");
-				$("#failure-modal").modal("show");
-			}
-		});
+		if ($("#checking").val() != 5) {
+			$form.find(".form-group:nth-child(3)").after("<div class='col-md-6 col-md-offset-6 danger-msg'><p class='text-danger'>Введите верное значение</p></div>");
+			setTimeout(function() {$(".danger-msg").fadeOut()}, 3000);
+		} else {
+			$.ajax({
+				method: "POST",
+				url: sg_forms_ajax.ajax_url,
+				data: {
+					action: "sg_ajax",
+					nonce: sg_forms_ajax.nonce,
+					name: $("#name").val(),
+					phone: $("#phone2").val(),
+					checking: $("#checking").val(),
+					agreement: $("#agreement").val(),
+					form_type: $form.find(".form-type").val()
+				},
+				success: function() {
+					$form.trigger("reset");
+					$("#callback-form").modal("hide");
+					$("#success-modal").modal("show");
+				},
+				error: function() {
+					$form.trigger("reset");
+					$("#callback-form").modal("hide");
+					$("#failure-modal").modal("show");
+				}
+			});
+		}
 	});
 });
