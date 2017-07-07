858ef3840009ed6c2b51cd021f94d1b4dc8d4204
it4joy <drkierkegor@gmail.com>
2017-07-07 19:57:44 +0300

    2017-07-07 :: 19:57

---
+++ wp-content/themes/sv-theme/page.php
@@ -0,0 +1,35 @@
+<?php get_header(); ?>
+
+<div class="container">
+	<div class="row">
+		<aside class="col-md-3">
+			<?php get_sidebar(); ?>
+		</aside>
+
+		<main class="col-md-9">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="breadcrumbs-wrapper">
+						<?php if ( function_exists('kama_breadcrumbs') ) kama_breadcrumbs(); ?>
+					</div>
+				</div>
+			</div>
+
+			<div class="row double-heading">
+				<div class="col-md-12">
+					<h3 class="uppercase"><?php the_title(); ?></h3>
+				</div>
+			</div>
+
+			<div class="row">
+				<?php the_post(); ?>
+
+				<div class="col-md-12">
+					<?php the_content(); ?>
+				</div>
+			</div>
+		</main>
+	</div>
+</div>
+
+<?php get_footer(); ?>
